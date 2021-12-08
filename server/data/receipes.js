e2343c1171d3ec2ec71993b3f33f3e8a5b45ad2d
songhany <ysh961008@yeah.net>
2021-12-07 21:23:14 -0500

    Rebuild server

--- server/data/receipes.js
+++ server/data/receipes.js
@@ -1,16 +1,29 @@
 const axios = require('axios');
-// receipe API key
-const app_id = '1b34c8e3';
-const app_key = '881547ba769f1c613438da84da47eb64';
+const apiKey = '01378051ff2c4d99846649b53b91835a';  // receipe API key  https://spoonacular.com/food-api/console#Profile
 
 let exportedMethods = {
 
-  async getRecipesById(id) {  // https://api.edamam.com/api/recipes/v2/b79327d05b8e5b838ad6cfd9576b30b6?type=public&app_id=1b34c8e3&app_key=881547ba769f1c613438da84da47eb64
+  async getRecipeAPI() {
+    const { data } = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`);
+    const parsedData = data;
+    return parsedData
+  },
+
+  async getReceipesPageList(offset, number) {
+    if (offset === undefined || number === undefined) throw "The offset or number parameter does not exit";
+    const baseUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&offset=${offset}&number=${number}`;
+    // console.log(baseUrl);
+    const { data } = await axios.get(baseUrl);
+    const parsedData = data;
+    return parsedData;
+  },
+
+  async getRecipesById(id) {  https://api.spoonacular.com/recipes/complexSearch?apiKey=01378051ff2c4d99846649b53b91835a&offset=0&number=30
     try {
       if (id === undefined) throw "The id parameter does not exit";
-
-      const baseUrl = `https://api.edamam.com/api/recipes/v2/` + id + `?type=public&app_id=` + app_id + `&app_key=` + app_key;
-      console.log(baseUrl);
+      
+      const baseUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
+      // console.log(baseUrl);
       const { data } = await axios.get(baseUrl); // id = b79327d05b8e5b838ad6cfd9576b30b6 
       const parsedData = data; // parse the data from JSON into a normal JS Object
       return parsedData; // this will be the single Pokemon object
@@ -19,13 +32,12 @@ let exportedMethods = {
     }
   },
 
-
-  async getRecipesBySearchTerm(term) {
+  async getRecipesByTerm(term) {
     if (term === undefined || term === null) throw "The query term is undefined";
     if (typeof term !== 'string') term = term.toString();
 
     try {
-      const { data } = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${term}&app_id=${app_id}&app_key=${app_key}`);
+      const { data } = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${term}`);
       const parsedData = data;  // parse the data from JSON into a normal JS Object
       if (!parsedData) throw `We're sorry, but no results were found for ${term}.`;
 
@@ -35,19 +47,14 @@ let exportedMethods = {
     }
   },
 
-
-  async getRandomRecipesBySearchTerm(term) {
-    if (term === undefined || term === null) throw "The query term is undefined";
-    if (typeof term !== 'string') term = term.toString();
-
+  async getRandomRecipes() {
     try {
-      const { data } = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${term}&app_id=${app_id}&app_key=${app_key}&random=true`);
+      const { data } = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`);
       const parsedData = data;  // parse the data from JSON into a normal JS Object
-      if (!parsedData) throw `We're sorry, but no results were found for ${term}.`;
-
+      // if (!parsedData) throw `We're sorry, but no results were found for ${term}.`;
       return parsedData;
     } catch (e) {
-      throw `No results were found for search term: ${term}`;
+      console.log(e);
     }
   },
 }
--- server/routes/receipe.js
+++ server/routes/receipe.js
@@ -12,6 +12,33 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 
+router.get('/page/:page', async (req, res) => {
+  if (!req.params.page) throw 'You must specify a page number to get';
+
+  try {
+    let cacheOfReceipePage = await client.getAsync(`receipePage${req.params.page}`);
+    if (cacheOfReceipePage) {
+      res.send(JSON.parse(cacheOfReceipePage));
+    } else {
+      let offset = 0;
+      let limit = 30;
+
+      if (req.params.page) {
+        offset = parseInt(req.params.page) * limit;
+      }
+
+      if (parseInt(offset) < 0 || parseInt(limit) < 0) throw "Page parameters for offset and limit must be positive number"
+      
+      const receipePage = await receipeData.getReceipesPageList(parseInt(offset), parseInt(limit));
+      res.json(receipePage);
+      let cacheReceipePage = await client.setAsync(`receipePage${req.params.page}`, JSON.stringify(receipePage));  // store receipePage in cache
+    }
+  } catch (e) {
+    res.status(404).json({ message: 'Server /page/:page Error. Receipe Page not found' });
+  }
+})
+
+
 router.get('/:id', async (req, res) => {
     try {
       if (!req.params.id) throw 'You must specify a receipeId to get';
@@ -46,18 +73,18 @@ router.post('/search', async (req, res) => {
       const existReceipe = await client.getAsync(`${searchTerm}`);
       await client.zincrbyAsync('receipeSortedSet', 1, `${searchTerm}`); 
       // console.log(existReceipe);
-      res.json(existReceipe);
+      res.json(JSON.parse(existReceipe));
     }
     else {
     // If the term is not in the set, you will add it to the set and set the initial value to 1. 
     // query the API endpoint for the search term
-      const searchedReceipe = await receipeData.getRecipesBySearchTerm(searchTerm);  
+      const searchedReceipe = await receipeData.getRecipesByTerm(searchTerm);  
       let setReceipe = await client.setAsync(`${searchTerm}`,  JSON.stringify(searchedReceipe));
       let newCacheSearchedReceipe  = await client.getAsync(`${searchTerm}`);
 
       let addToSortedSort = await client.zaddAsync('receipeSortedSet', 1, `${searchTerm}`);
 
-      res.json(searchedReceipe);
+      res.json(JSON.parse(searchedReceipe));
     }
   } catch (e) {
     res.status(404).json({ error: `Server /receipe/search Error. No results were found with search term: ${searchTerm}` });
@@ -65,7 +92,7 @@ router.post('/search', async (req, res) => {
 })
 
 
-router.get('/popularsearches', async (req, res) => {
+router.get('/popularSearches', async (req, res) => {
   try {
     let topPopularTerm = await client.zrevrangeAsync('receipeSortedSet', 0, 9);
     // res.render('others/popularSearch', {popularTerms: topPopularTerm, title: `Top 10 Popular Term`}); 
@@ -76,17 +103,9 @@ router.get('/popularsearches', async (req, res) => {
 });
 
 
-
-router.post('/randomSearch', async (req, res) => {
-  try {
-    let searchTerm = 'chicken';
-
-    if (!searchTerm || searchTerm == ' ') {  //give a response status code of 400 on the page, and render an HTML page with a paragraph class called error
-      res.status(400);
-      throw '"You should input text into form for searching and the text cannot just be spaces.';
-    }
-      
-    const randomReceipe = await receipeData.getRandomRecipesBySearchTerm(searchTerm);  
+router.get('/randomSearch', async (req, res) => {
+  try {      
+    const randomReceipe = await receipeData.getRandomRecipes();  
     res.json(randomReceipe);
   } catch (e) {
     console.log(e);
