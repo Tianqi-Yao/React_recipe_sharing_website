2e8f3a3b4fbf9bea490c078623d21591cfc0a169
dccif <heyujieboy@163.com>
2021-12-16 10:48:43 -0500

    login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,77 @@
 import React from 'react'
 // import logo from './img/pokeBall.png';
-import logo from './logo.svg';
-import './App.css';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css'
 
-import Home from './components/Home';
-import TodoList from './components/Todo';
-import ReceipePage from './components/ReceipePage';
-import Receipe from './components/Receipe';
-import CreateReceipe from './components/CreateReceipe';
-import UpdateReceipe from './components/UpdateReceipe';
-
-import PopularSearch from './components/PopularSearch';
+import Home from './components/Home'
+import TodoList from './components/Todo'
+import ReceipePage from './components/ReceipePage'
+import Receipe from './components/Receipe'
+import CreateReceipe from './components/CreateReceipe'
+import UpdateReceipe from './components/UpdateReceipe'
+import Signup from "./components/Signup"
+import Login from "./components/Login"
+// import PopularSearch from './components/PopularSearch'
 // import ReceipeSearch from './components/ReceipeSearch';
 // import Header from './components/Header'
-import UserProfile from './components/UserProfile';
-import UserRecipe from './components/UserRecipe';
-import EditProfile from './components/EditProfile';
-import {
-  BrowserRouter as Router,
-  Link,
-  Switch,
-  Route,
-  NavLink
-} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom"
+import {AuthProvider} from "./contexts/AuthContext"
+import ForgotPassword from "./components/ForgotPassword"
+import PrivateRoute from "./components/PrivateRoute"
+import UpdateProfile from "./components/UpdateProfile"
+import {Container} from "react-bootstrap"
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Recipes Sharing Platform</h1>
-          <p className="hometext">
-            The project is for people to make and find tasty foods' receipe. Users can see other users’ sharing information on this platform. 
-            Users can find and upload their recipe as well as use a task tracker to plan buying different ingredients. 
-          </p>
-          <Link className="showlink" to="/">Home</Link>
-          <Link className="showlink" to="/todo">TodoList</Link>
-          <Link className="showlink" to="/receipe/page/0">ReceipePage</Link>
-          <Link className="showlink" to="/createReceipe">Create Receipe</Link>
-          <Link className="showlink" to="/updateReceipe">Update Receipe</Link>
-        </header>
-        <br />
-        <br />
-        <div className="App-body">  {/* "/shows/page/:pagenum" will generate 'props.match.params.pagenum' and pass it to "components" */}
-          <Route exact path="/" component={Home} />
-          <Route exact path="/todo" component={TodoList} />
-          <Route exact path="/receipe/page/:page" component={ReceipePage} />
-          <Route exact path="/receipe/:id" component={Receipe} />
-          <Route exact path="/createReceipe" component={CreateReceipe} />
-          <Route exact path="/updateReceipe" component={UpdateReceipe} />
-          {/* <Route exact path="/search" component={ReceipeSearch} /> */}
-        </div>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                    <h1 className="App-title">Recipes Sharing Platform</h1>
+                    <p className="hometext">
+                        The project is for people to make and find tasty foods' receipe. Users can see other users’
+                        sharing information on this platform.
+                        Users can find and upload their recipe as well as use a task tracker to plan buying different
+                        ingredients.
+                    </p>
+                    <Link className="showlink" to="/">Home</Link>
+                    <Link className="showlink" to="/todo">TodoList</Link>
+                    <Link className="showlink" to="/receipe/page/0">ReceipePage</Link>
+                    <Link className="showlink" to="/createReceipe">Create Receipe</Link>
+                    <Link className="showlink" to="/updateReceipe">Update Receipe</Link>
+                    <Link className="showlink" to="/signup">Signup</Link>
+                    <Link className="showlink" to="/login">Login</Link>
+                </header>
+                <br/>
+                <br/>
+                <div
+                    className="App-body">  {/* "/shows/page/:pagenum" will generate 'props.match.params.pagenum' and pass it to "components" */}
+                    <Switch>
+                        <React.Fragment>
+                            <Route exact path="/" component={Home}/>
+                            <Route exact path="/todo" component={TodoList}/>
+                            <Route exact path="/receipe/page/:page" component={ReceipePage}/>
+                            <Route exact path="/receipe/:id" component={Receipe}/>
+                            <Route exact path="/createReceipe" component={CreateReceipe}/>
+                            <Route exact path="/updateReceipe" component={UpdateReceipe}/>
+                            <Container
+                                className="border-0 d-flex align-items-center justify-content-center"
+                                style={{minHeight: "100vh"}}>
+                                <div className="w-100" style={{maxWidth: "400px"}}>
+                                    <AuthProvider>
+                                        <Route exact path="/signup" component={Signup}/>
+                                        <Route exact path="/login" component={Login}/>
+                                        <Route path="/forgot-password" component={ForgotPassword}/>
+                                        <PrivateRoute path="/update-profile" component={UpdateProfile}/>
+                                    </AuthProvider>
+                                </div>
+                            </Container>
+                            {/* <Route exact path="/search" component={ReceipeSearch} /> */}
+                        </React.Fragment>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -1,63 +1,64 @@
-const dbConnection = require('../config/mongoConnection');
-const data = require('../data/');
-const users = data.users;
-const posts = data.posts;
+const dbConnection = require('../config/mongoConnection')
+const data = require('../data/')
+const users = data.users
+const posts = data.posts
+const recipes = data.receipes
 
 async function main() {
-  const db = await dbConnection();
+    const db = await dbConnection()
 
-  await db.dropDatabase();
+    await db.dropDatabase()
 
-  const patrick = await users.addUser('Patrick', 'Hill');
-  const id = patrick._id;
+    const patrick = await users.addUser('Patrick', 'Hill')
+    const id = patrick._id
 
-  await posts.addPost('Hello, class!', 'Today we are creating a blog!', [], id);
+    await posts.addPost('Hello, class!', 'Today we are creating a blog!', [], id)
 
-  await posts.addPost(
-    'Using the seed',
-    'We use the seed to have some initial data so we can just focus on servers this week',
-    [],
-    id
-  );
-  await posts.addPost(
-    'Using routes',
-    'The purpose of today is to simply look at some GET routes',
-    [],
-    id
-  );
+    await posts.addPost(
+        'Using the seed',
+        'We use the seed to have some initial data so we can just focus on servers this week',
+        [],
+        id
+    )
+    await posts.addPost(
+        'Using routes',
+        'The purpose of today is to simply look at some GET routes',
+        [],
+        id
+    )
 
-  const aiden = await users.addUser('Aiden', 'Hill');
-  await posts.addPost(
-    "Aiden's First Post",
-    "I'm 6 months old, I can't blog1",
-    [],
-    aiden._id
-  );
-  await posts.addPost(
-    "Aiden's Second Post",
-    "I'm still 6 months old, I told you already, I can't blog1",
-    [],
-    aiden._id
-  );
+    const aiden = await users.addUser('Aiden', 'Hill')
+    await posts.addPost(
+        "Aiden's First Post",
+        "I'm 6 months old, I can't blog1",
+        [],
+        aiden._id
+    )
+    await posts.addPost(
+        "Aiden's Second Post",
+        "I'm still 6 months old, I told you already, I can't blog1",
+        [],
+        aiden._id
+    )
 
-    let p1 = await recipes.addPost("1", "111");
-    let p2 = await recipes.addPost("1", "111");
-    let p3 = await recipes.addPost("1", "111");
-    let p4 = await recipes.addPost("1", "111");
+    let p1 = await recipes.addPost("1", "111")
+    let p2 = await recipes.addPost("1", "111")
+    let p3 = await recipes.addPost("1", "111")
+    let p4 = await recipes.addPost("1", "111")
 
-    let u1 = await users.addUser("user", "123", [p1._id, p2._id, p3._id, p4._id], [p1._id, p2._id, p3._id, p4._id]);
-    console.log(u1);
+    let u1 = await users.addUser("user", "123", [p1._id, p2._id, p3._id, p4._id], [p1._id, p2._id, p3._id, p4._id])
+    console.log(u1)
 
-    await db.serverConfig.close();
-    console.log('Done!');
+    await db.serverConfig.close()
+    console.log('Done!')
 
-  console.log('Done seeding database');
-  await db.serverConfig.close();
+    console.log('Done seeding database')
+    await db.serverConfig.close()
 }
 
 main().catch((error) => {
-  console.error(error);
-  return dbConnection().then((db) => {
-    return db.serverConfig.close();
-  });
-});
+    console.error(error)
+    return dbConnection().then((db) => {
+        return db.serverConfig.close()
+    })
+})
