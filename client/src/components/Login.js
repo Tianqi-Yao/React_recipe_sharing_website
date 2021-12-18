12e1c213d35e4143e3918c7373a2a7bc7ac9a8aa
dccif <heyujieboy@163.com>
2021-12-17 21:06:10 -0500

    check user in login and signup page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,12 +2,12 @@ import React, {useRef, useState} from "react"
 import {Alert, Button, Card, Form} from "react-bootstrap"
 import axios from "axios"
 import {useAuth} from "../contexts/AuthContext"
-import {Link, useHistory} from "react-router-dom"
+import {Link, Redirect, useHistory} from "react-router-dom"
 
 export default function Login() {
     const emailRef = useRef()
     const passwordRef = useRef()
-    const {login, googleSignInWithPopup} = useAuth()
+    const {login, googleSignInWithPopup, currentUser} = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const history = useHistory()
@@ -43,35 +43,36 @@ export default function Login() {
     }
 
     return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Log In</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required/>
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required/>
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">
-                            Log In
+        currentUser ? <Redirect to={`/userprofile/${currentUser.uid}`}/> :
+            <>
+                <Card>
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Log In</h2>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group id="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required/>
+                            </Form.Group>
+                            <Form.Group id="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef} required/>
+                            </Form.Group>
+                            <Button disabled={loading} className="w-100" type="submit">
+                                Log In
+                            </Button>
+                        </Form>
+                        <Button onClick={handleGoogleSign}>
+                            Google Login
                         </Button>
-                    </Form>
-                    <Button onClick={handleGoogleSign}>
-                        Google Login
-                    </Button>
-                    <div className="w-100 text-center mt-3">
-                        <Link to="/forgot-password">Forgot Password?</Link>
-                    </div>
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2">
-                Need an account? <Link to="/signup">Sign Up</Link>
-            </div>
-        </>
+                        <div className="w-100 text-center mt-3">
+                            <Link to="/forgot-password">Forgot Password?</Link>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <div className="w-100 text-center mt-2">
+                    Need an account? <Link to="/signup">Sign Up</Link>
+                </div>
+            </>
     )
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from "react"
 import {Alert, Button, Card, Form} from "react-bootstrap"
 import {useAuth} from "../contexts/AuthContext"
-import {Link, useHistory} from "react-router-dom"
+import {Link, Redirect, useHistory} from "react-router-dom"
 import axios from "axios"
 
 export default function Signup() {
@@ -9,7 +9,7 @@ export default function Signup() {
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
     const userNameRef = useRef()
-    const {signup, getCurrentUser} = useAuth()
+    const {signup, getCurrentUser, currentUser} = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const history = useHistory()
@@ -53,37 +53,38 @@ export default function Signup() {
     }
 
     return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit} onChange={handleChange}>
-                        <Form.Group id="username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="username" ref={userNameRef} required/>
-                        </Form.Group>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required/>
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required/>
-                        </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required/>
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">
-                            Sign Up
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2">
-                Already have an account? <Link to="/login">Log In</Link>
-            </div>
-        </>
+        currentUser ? <Redirect to={`/userprofile/${currentUser.uid}`}/> :
+            <>
+                <Card>
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Sign Up</h2>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form onSubmit={handleSubmit} onChange={handleChange}>
+                            <Form.Group id="username">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="username" ref={userNameRef} required/>
+                            </Form.Group>
+                            <Form.Group id="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required/>
+                            </Form.Group>
+                            <Form.Group id="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef} required/>
+                            </Form.Group>
+                            <Form.Group id="password-confirm">
+                                <Form.Label>Password Confirmation</Form.Label>
+                                <Form.Control type="password" ref={passwordConfirmRef} required/>
+                            </Form.Group>
+                            <Button disabled={loading} className="w-100" type="submit">
+                                Sign Up
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+                <div className="w-100 text-center mt-2">
+                    Already have an account? <Link to="/login">Log In</Link>
+                </div>
+            </>
     )
 }
